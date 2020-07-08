b566b21f0452550f2a8ccc76e097d68d489b431e
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-07-08 16:25:22 -0500

    uploading images

--- src/containers/Articles/index.js
+++ src/containers/Articles/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import Articles from "../../components/Articles";
 import Query from "../../components/Query";
 import ARTICLES_QUERY from "../../queries/article/articles";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
     <div>
       <div className="uk-section">
-        <div className="uk-container uk-container-large">
+        <Container className="uk-container uk-container-large">
           <h1>Strategic Management</h1>
           <Query query={ARTICLES_QUERY}>
             {({ data: { articles } }) => {
               return <Articles articles={articles} />;
             }}
           </Query>
-        </div>
+        </Container>
       </div>
     </div>
   );
